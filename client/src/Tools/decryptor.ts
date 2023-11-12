10b670c26132232ec561a2b63a4fe169f4971935
nghia.duong272919136 <nghia.duong272919136@hcmut.edu.vn>
2023-11-12 13:45:40 +0700

    Update decryptor.ts

--- client/src/Tools/decryptor.ts
+++ client/src/Tools/decryptor.ts
@@ -1,6 +1,8 @@
 import key from "./RSAKey.tool";
 
-export default function decryptor(data:string): any
+export default function decryptor(data: string): any
 {
-      return JSON.parse(key.decrypt(data) as string);
+      const decryptedData: string | boolean = key.decrypt(data);
+      if (decryptedData === false) return null;
+      return JSON.parse(decryptedData);
 }
