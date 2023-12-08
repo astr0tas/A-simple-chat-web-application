9a4496da0d3dedf5475eac8f7e105f720febec0b
nghia.duong272919136 <nghia.duong272919136@hcmut.edu.vn>
2023-12-08 13:51:28 +0700

    minor changes

--- client/src/config/serverDomain.config.ts
+++ client/src/config/serverDomain.config.ts
@@ -1,3 +1,3 @@
-const domain: string = 'https://192.168.1.111:8080';
+const domain: string = 'https://localhost:8080';
 
 export default domain;
--- server/src/config/domain.config.ts
+++ server/src/config/domain.config.ts
@@ -1,3 +1,5 @@
-const domain: any = ["https://localhost:3000"];
+const domain: any = [
+  "https://localhost:3000"
+];
 
 export default domain;
